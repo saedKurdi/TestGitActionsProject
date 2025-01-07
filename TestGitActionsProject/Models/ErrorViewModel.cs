2e0bcf830c83e72165bead5a76f89d1c9a84f344
Saed Mamedov <seid13463@gmail.com>
2025-01-07 19:12:36 +0400

    test git-actions commit

---
+++ TestGitActionsProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TestGitActionsProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
