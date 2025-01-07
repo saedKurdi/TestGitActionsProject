b6ad378b5cffc4773692804aa9013543878d39c6
Saed Mamedov <seid13463@gmail.com>
2025-01-07 23:01:45 +0400

    done change .

--- TestGitActionsProject/obj/Debug/net8.0/TestGitActionsProject.AssemblyInfo.cs
+++ TestGitActionsProject/obj/Debug/net8.0/TestGitActionsProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestGitActionsProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+305a6018d4feec2d7fada4519ca85f1d58ee161a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestGitActionsProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestGitActionsProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
